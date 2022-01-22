1226d05f22d0489fb3df95dea91ac79217714bd8
BanhMiNgot45 <stevenhle459@gmail.com>
2022-01-22 12:23:05 -0600

    Added randomness to oscillator

--- Assets/Scripts/Oscillator.cs
+++ Assets/Scripts/Oscillator.cs
@@ -9,19 +9,23 @@ public class Oscillator : MonoBehaviour
     public bool x = false;
     public bool y = true;
     public bool z = false;
+    public bool random = true;
     private float time = 0f;
+    private float seed = 0f;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        if (random) {
+            seed = Random.Range(-2 * Mathf.PI, 2* Mathf.PI);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
         time += Time.deltaTime;
-        float newPos = amplitude * Mathf.Sin(frequency * time);
+        float newPos = amplitude * Mathf.Sin(frequency * time + seed);
         if (x) {
             transform.position += new Vector3(newPos, 0, 0); 
         }

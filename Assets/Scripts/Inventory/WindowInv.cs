f76b4380132513fb8ff415f552de0a864ac4119a
J <joscschinn@gmail.com>
2022-04-30 09:28:12 -0500

    Yay stuff

--- Assets/Scripts/Inventory/WindowInv.cs
+++ Assets/Scripts/Inventory/WindowInv.cs
@@ -2,22 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ArmorInv : MonoBehaviour
+public class WindowInv : MonoBehaviour
 {
-    public GameObject inventory;
     // Start is called before the first frame update
     void Start()
     {
-
-
+        
     }
 
     // Update is called once per frame
     void Update()
     {
         RectTransform rect;
-        rect = inventory.GetComponent<RectTransform>();
-        rect.offsetMin = new Vector2((float)Screen.width / 3f, (float)Screen.height *(5f/10f));//Left Bottom
+        rect = this.GetComponent<RectTransform>();
+        rect.offsetMin = new Vector2((float)Screen.width / 3f, (float)Screen.height * (5f / 10f));//Left Bottom
         rect.offsetMax = new Vector2(-100, -15);//Right Top
 
     }
